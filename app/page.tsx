e5082290e4cd1b9f684b45244e37858e11598ef4
Manh Dao <07mandao@skola.boras.se>
2026-02-22 15:02:56 +0100

    added the mervärde sektion to the digital productpasses, which include and ddiscover more section and an discover new collection section

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import ProductImage from "@/app/component/productImage";
 import ThreeKeyFacts from "@/app/component/threeKeyFacts";
 import DropdownModule from "./component/popDownInfo";
 import MinimalFooter from "./component/minimalFooter";
+import ProductCard from "./component/productCard";
+import ProductSlider from "./component/productSlider";
 export default function Home() {
   const listofFacts = [
     { keyFact: "12km", value: "Sträcka" },
@@ -25,6 +27,30 @@ export default function Home() {
           />
           <ThreeKeyFacts listofFacts={listofFacts} />
           <DropdownModule />
+          <h2 className="text-3xl pl-2 mt-15 ">Kompletera din look med</h2>
+          <p className="pl-2 text-xl">See mer</p>
+          <ProductSlider />
+          <div className="bg-[#D4E9E2] p-2 mt-15 pt-10 pb-15 ">
+            <h1 className="text-3xl pb-5">Ny kollektion - Stay Iconic</h1>
+            <div className="flex justify-around">
+              <div className="border self-start px-2.5 py-1 ">Shoppa</div>
+              <div className="relative inline-block">
+                <Image
+                  src={"/twoGirls.webp"}
+                  alt="bild på två tejer"
+                  width={200}
+                  height={300}
+                />
+                <Image
+                  src={"/jackaiStudio.webp"}
+                  alt="Jacka i studio"
+                  width={100}
+                  height={100}
+                  className="absolute -bottom-10 -right-5 w-24 h-24 object-cover"
+                />
+              </div>
+            </div>
+          </div>
           <MinimalFooter />
         </DppLayout>
       </div>
