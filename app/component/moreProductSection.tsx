18593020832d59d259ecc1cd491fd4544a5121f1
Manh Dao <07mandao@skola.boras.se>
2026-02-23 08:42:45 +0100

    final changes

--- app/component/moreProductSection.tsx
+++ app/component/moreProductSection.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ProductSlider from "./productSlider";
+import Image from "next/image";
 function MoreProductSection() {
   return (
     <div>
-      {" "}
       <h2 className="text-3xl pl-2 mt-15 ">Kompletera din look med</h2>
-      <p className="pl-2 text-xl">See mer</p>
+      <p className="pl-2 text-xl inline-block pr-2">
+        <a
+          href="https://www.8848altitude.com/sv-SE/dam/skidklader/skidjackor"
+          target="_blank"
+        >
+          See mer
+        </a>
+      </p>
+      <a
+        href="https://www.8848altitude.com/sv-SE/dam/skidklader/skidjackor"
+        target="_blank"
+      >
+        <Image
+          className="inline-block"
+          src="/right-arrow.png"
+          height={10}
+          width={30}
+          alt="Right aroow "
+        />
+      </a>
       <ProductSlider />
     </div>
   );
--- app/component/newCollection.tsx
+++ app/component/newCollection.tsx
@@ -5,7 +5,14 @@ function NewCollection() {
     <div className="bg-[#D4E9E2] p-2 mt-15 pt-10 pb-15 ">
       <h1 className="text-3xl pb-5">Ny kollektion - Stay Iconic</h1>
       <div className="flex justify-around">
-        <div className="border self-start px-2.5 py-1 ">Shoppa</div>
+        <div className="border self-start px-2.5 py-1 ">
+          <a
+            href="https://www.8848altitude.com/en/8848-magazine"
+            target="_blank"
+          >
+            Shoppa
+          </a>
+        </div>
         <div className="relative inline-block">
           <Image
             src={"/twoGirls.webp"}
